7f7f9394ca5969e744cd0df6433eaf8827a58f00
bootcamp2024 <bruno.bm12@gmail.com>
2025-01-29 11:40:37 -0300

    Projeto criado e configurado application properties

---
+++ src/main/java/port/brunobraatz/lista_jogos/ListaJogosApplication.java
@@ -0,0 +1,13 @@
+package port.brunobraatz.lista_jogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/port/brunobraatz/lista_jogos/ListaJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package port.brunobraatz.lista_jogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
